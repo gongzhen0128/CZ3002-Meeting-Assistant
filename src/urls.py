f9bab13af81ff27dce0ef621dcb16021c592dcc2
zesheng <zeshengxie@gmail.com>
2018-09-22 21:45:38 +0800

    home

--- src/urls.py
+++ src/urls.py
@@ -21,7 +21,7 @@ from controll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='open'),
+    # path('', views.home, name='open'),
     path('home/', views.home, name='home'),
     path('login/', views.login, name='login'),
 
